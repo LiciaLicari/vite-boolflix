13f3f504dfd3d28a037ce66e04311bd93e8ed823
“LiciaLicari” <licianbsplicari@gmail.com>
2023-09-28 16:23:11 +0200

    add movie list + input&btn
    need fix

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <script>
+import { store } from './store.js'
 
+
+export default {
+  name: "App",
+  //emits: ['searchTitle'],
+
+  data() {
+    return {
+      store
+    }
+  },
+
+  mounted() {
+    this.store.fetchMovie(this.store.baseURL);
+  }
+
+
+}
 </script>
 
 <template>
-    <div class="container p-2">
-        <div class="d-flex align-items-center justify-content-center">
-            <input type="text" class="form-control">
-            <button class="ms-3 btn btn-primary">Search</button>
-        </div>
+  <div class="container p-2">
+    <div class="d-flex align-items-center justify-content-center" @submit.prevent="store.fetchMovie">
+      <input type="text" v-model="store.query" class="form-control" placeholder="Type here the title you are looking for">
+      <button class="ms-3 btn btn-primary @click.prevent="
+        placeholder="Type the title you are looking for">Search</button>
     </div>
+    <div class="row">
+      <div class="col-3">
+        <div class="card" v-for="movie in store.movieList">
+          <p>original title : {{ movie.original_title }}</p>
+          <p>title : {{ movie.title }}</p>
+          <p>language : {{ movie.original_language }}</p>
+          <p>rate : {{ movie.vote_average }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
 @use '../src/assets/scss/partials/variables' as *;
-
 </style>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,24 @@
-import { reactive } from vue;
-import axios from 'axios';
-export const state = reactive({
-    url: 'https://api.themoviedb.org/3/search/movie?api_key=2110a6167040b5168cc3ef4a80d12b25&query=',
+import axios from "axios";
+import { reactive } from "vue";
+export const store = reactive({
+    baseURL: 'https://api.themoviedb.org/3/search/movie?api_key=2110a6167040b5168cc3ef4a80d12b25&query=',
+    query: '',
+    querySpan: '',
+    movieList: null,
+
+    fetchMovie(url) {
+        axios
+            .get(url)
+            .then(response => {
+                //console.log('response...', response);
+                this.querySpan = this.query;
+                this.query = '';
+                console.log(response.data.results);
+                this.movieList = response.data.results;
+
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
 })
