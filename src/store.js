9b7aad0fa25ff39cbd90e330508550148c393791
“LiciaLicari” <licianbsplicari@gmail.com>
2023-10-02 15:22:05 +0200

    add tv serie

--- src/store.js
+++ src/store.js
@@ -2,9 +2,11 @@ import axios from "axios";
 import { reactive } from "vue";
 export const store = reactive({
     baseURL: 'https://api.themoviedb.org/3/search/movie?api_key=2110a6167040b5168cc3ef4a80d12b25',
+    tv_url: 'https://api.themoviedb.org/3/search/tv?api_key=e10a79c4628cb2c1d4e74a07721be7c3&language=it_IT&query=',
     query: '',
     querySpan: '',
     movieList: [],
+    tvSerieList: [],
 
     fetchMovie() {
         axios
@@ -17,6 +19,23 @@ export const store = reactive({
                 this.query = '';
                 console.log(response.data.results);
                 this.movieList = response.data.results;
+
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    },
+    fetchtvSerie(tv_url) {
+        axios
+            .get(this.tv_url, {params: {
+                query: this.query
+            }})
+            .then(response => {
+                //console.log('response...', response);
+                this.querySpan = this.query;
+                this.query = '';
+                console.log(response.data.results);
+                this.tvSerieList = response.data.results;
 
             })
             .catch(error => {
